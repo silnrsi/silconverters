4ef95b0f6ece12cad21f601e0589f71f8756c4fd
beaton <beaton@localhost>
2013-08-02 09:17:11 -0500

    added support for Bulk SFM converter, Jump2Toolbox (for CscDot) and removed the COM interop-ness of the SILConvertersOffice assemblies

---
+++ src/Jump2Toolbox/Jump2Toolbox.cpp
@@ -0,0 +1,10 @@
+// Jump2Toolbox.cpp : Implementation of DLL Exports.
+
+#include "stdafx.h"
+#include "resource.h"
+
+// The module attribute causes DllMain, DllRegisterServer and DllUnregisterServer to be automatically implemented for you
+[ module(dll, uuid = "{7CF02E92-A3FB-40D3-AE72-E87E0926680B}", 
+		 name = "Jump2Toolbox", 
+		 helpstring = "Jump2Toolbox 1.0 Type Library",
+		 resource_name = "IDR_JUMP2TOOLBOX") ];
