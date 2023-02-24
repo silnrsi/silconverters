b7a1123502c1af85c5c34099ba0d9890f815ccf6
bobeaton <bob_eaton@sall.com>
2023-02-24 15:40:20 -0600

    missed a file that had changes for moving the pin to top to the control

--- src/ParatextPluginBackTranslationHelper/BackTranslationHelperForm.Designer.cs
+++ src/ParatextPluginBackTranslationHelper/BackTranslationHelperForm.Designer.cs
@@ -30,7 +30,6 @@ namespace SIL.ParatextBackTranslationHelperPlugin
         private void InitializeComponent()
         {
             this.backTranslationHelperCtrl = new BackTranslationHelper.BackTranslationHelperCtrl();
-            this.buttonPinToTop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // backTranslationHelperCtrl
@@ -49,24 +48,11 @@ namespace SIL.ParatextBackTranslationHelperPlugin
             this.backTranslationHelperCtrl.Size = new System.Drawing.Size(790, 449);
             this.backTranslationHelperCtrl.TabIndex = 1;
             // 
-            // buttonPinToTop
-            // 
-            this.buttonPinToTop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonPinToTop.Image = global::SIL.ParatextBackTranslationHelperPlugin.Properties.Resources.pinup;
-            this.buttonPinToTop.Location = new System.Drawing.Point(768, 0);
-            this.buttonPinToTop.Name = "buttonPinToTop";
-            this.buttonPinToTop.Size = new System.Drawing.Size(32, 32);
-            this.buttonPinToTop.TabIndex = 2;
-            this.buttonPinToTop.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.buttonPinToTop.UseVisualStyleBackColor = true;
-            this.buttonPinToTop.Click += new System.EventHandler(this.ButtonPinToTop_Click);
-            // 
             // BackTranslationHelperForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.buttonPinToTop);
             this.Controls.Add(this.backTranslationHelperCtrl);
             this.Name = "BackTranslationHelperForm";
             this.Text = "Back Translating from {0} - {1} in verse: {2}";
@@ -80,6 +66,5 @@ namespace SIL.ParatextBackTranslationHelperPlugin
         #endregion
 
         private BackTranslationHelper.BackTranslationHelperCtrl backTranslationHelperCtrl;
-        private System.Windows.Forms.Button buttonPinToTop;
     }
 }
