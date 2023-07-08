2fffce23f8a64df87aef6af8faea42d599f4b656
bobeaton <bob_eaton@sall.com>
2023-07-08 16:35:39 -0500

    when ignoring inline text data (e.g. footnotes), translate the 'fq' marker also (the quoted text of a footnote)

--- src/ParatextPluginBackTranslationHelper/Properties/Settings.Designer.cs
+++ src/ParatextPluginBackTranslationHelper/Properties/Settings.Designer.cs
@@ -98,10 +98,13 @@ namespace SIL.ParatextBackTranslationHelperPlugin.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("<?xml version=\"1.0\" encoding=\"utf-16\"?>\r\n<ArrayOfString xmlns:xsd=\"http://www.w3." +
-            "org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\">\r\n  <s" +
-            "tring>ft</string>\r\n  <string>rq</string>\r\n  <string>s</string>\r\n</ArrayOfString>" +
-            "")]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
+<ArrayOfString xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"">
+  <string>fq</string>
+  <string>ft</string>
+  <string>rq</string>
+  <string>s</string>
+</ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection AdditionalMarkersToTranslate {
             get {
                 return ((global::System.Collections.Specialized.StringCollection)(this["AdditionalMarkersToTranslate"]));
