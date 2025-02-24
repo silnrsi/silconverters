0a81341d91874513bf257358e3c4977bcf3f3259
bobeaton <bob_eaton@sall.com>
2025-02-24 15:09:54 -0600

    update to 0.9.4 of eccore

--- src/ClipboardEC/AssemblyInfo.cs
+++ src/ClipboardEC/AssemblyInfo.cs
@@ -34,7 +34,7 @@ using System.Resources;	// GuidAttribute
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyVersion("5.3.2.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -64,6 +64,6 @@ using System.Resources;	// GuidAttribute
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.3.2.0")]
 [assembly: ComVisibleAttribute(false)]
 [assembly: NeutralResourcesLanguageAttribute("en")]
