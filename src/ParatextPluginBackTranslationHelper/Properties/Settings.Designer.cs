cf492ab35654953992f3655dd561fd260a0e838b
bobeaton <beaton@bootbarn.com>
2026-06-14 17:49:44 -0500

    added \s1 to the list of markers to translate by default for the BackTranslationHelper (since I met someone who wants to use that) at EMDC.

--- src/ParatextPluginBackTranslationHelper/Properties/Settings.Designer.cs
+++ src/ParatextPluginBackTranslationHelper/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SIL.ParatextBackTranslationHelperPlugin.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.13.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.14.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -106,6 +106,7 @@ namespace SIL.ParatextBackTranslationHelperPlugin.Properties {
   <string>xt</string>
   <string>rq</string>
   <string>s</string>
+  <string>s1</string>
   <string>ms1</string>
 </ArrayOfString>")]
         public global::System.Collections.Specialized.StringCollection AdditionalMarkersToTranslate {
